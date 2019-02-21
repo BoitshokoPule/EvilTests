21a390977f00109226161d8f9080d4aef3471d6d
Boitshoko <puleboitshoko96@gmail.com>
2019-02-21 17:11:54 +0200

    Changes committed after tests

--- Problems.Tests/UnitTest1.cs
+++ Problems.Tests/UnitTest1.cs
@@ -1,4 +1,5 @@
 using NUnit.Framework;
+using Problems;
 
 namespace Tests
 {
@@ -12,7 +13,32 @@ namespace Tests
         [Test]
         public void Test1()
         {
-            Assert.Pass();
+            Program j= new Program();
+            string output1= j.Prefix("Where are you????");
+            Assert.AreEqual("17,3:Where are you????",output1);
         }
+         [Test]
+        public void Test2()
+        {
+            Program p= new Program();
+            string output2= p.Prefix(null);
+            Assert.AreEqual(null,output2);
+        }
+         [Test]
+        public void Test3()
+        {
+            Program b= new Program();
+            string output3= b.Prefix("");
+            Assert.AreEqual("0,0",output3);
+        }
+          [Test]
+        public void Test4()
+        {
+            Program i= new Program();
+            string output4= i.Prefix("Hi");
+            Assert.AreEqual("2,1:Hi", output4);
+        }
+
+       
     }
 }
--- Problems/Program.cs
+++ Problems/Program.cs
@@ -2,8 +2,26 @@
 
 namespace Problems
 {
-    class Program
+    public class Program
     {
+        public string Prefix(string input)
+        {
+            if(input== null)
+            {
+               return null;
+            }
+         else if (input=="")
+        {
+                return "0,0";
+        }
+        else
+        {
+              int length= input.Length;
+              string[] arrayOfWords= input.Split(' ');
+              int numOfWords= arrayOfWords.Length;
+              return $"{length},{numOfWords}:{input}";
+        }
+        }
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
